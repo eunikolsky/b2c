e02053c1ef68e461a416cf0140d5b790da075927
Eugene <e@egeek.me>
2021-11-23 07:57:13 +0200

    Add primitive vcardsParser
    For some reason, it returns the count of one on my sample file with a
    number of contacts.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib where
+
+import Data.Functor (void)
+import Data.Text (Text)
+import Data.Void
+
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+type Parser = Parsec Void Text
+
+vcardParser :: Parser ()
+vcardParser = do
+  string "BEGIN:VCARD" *> eol
+  (void (string "END:VCARD") <|> void(some contentline)) *> eol
+  pure ()
+
+  where
+    contentline = some printChar <?> "contentline"
+
+vcardsParser :: Parser Int
+vcardsParser = length <$> some vcardParser
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
