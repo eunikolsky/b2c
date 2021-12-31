57e5f62511817f52aff4aa82ea0ae0a92f4f8fe4
Eugene <e@egeek.me>
2021-12-31 12:07:09 +0200

    Exit with error code 1 on parser errors
    This change requires moving the `printB2C` from the library to the
    application code because it doesn't feel right for a library to know
    about exit codes.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
-import qualified Data.Text.IO as T (getContents)
+import qualified Data.ByteString.Lazy as BSL (putStr)
+import qualified Data.Text as T (Text)
+import qualified Data.Text.IO as T (getContents, hPutStr)
+import System.Exit (ExitCode(ExitFailure), exitWith)
+import System.IO (stderr)
 
-import Lib (printB2C)
+import Lib (b2c)
 
 main :: IO ()
 main = T.getContents >>= printB2C
+
+-- |Wrapper around `b2c` that prints the calendar representation to `stdout`
+-- or the errors to `stderr` (and exits with error code 1 in this case).
+printB2C :: T.Text -> IO ()
+printB2C input = either printErrors BSL.putStr =<< b2c input
+  where
+    printErrors :: T.Text -> IO ()
+    printErrors errors = T.hPutStr stderr errors *> exitWith (ExitFailure 1)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,9 @@
 module Lib
   ( b2c
-  , printB2C
   ) where
 
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import System.IO (stderr)
 
 import Text.Megaparsec
 
@@ -24,8 +21,3 @@ b2c input = do
       pure $ Right output
 
     Left errors -> pure . Left . T.pack $ errorBundlePretty errors
-
--- |Wrapper around `b2c` that prints the calendar representation to `stdout`
--- or the errors to `stderr`.
-printB2C :: T.Text -> IO ()
-printB2C input = either (T.hPutStr stderr) BSL.putStr =<< b2c input
